d9e690dc79c44457cd44f3de02315f3cd47ddb12
shizuka-iot <tatakitubusu@gmail.com>
2021-01-31 15:33:16 +0900

    ヘッダーのロゴをトップへのリンクにした

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,9 @@
 
 <header class="center column">
 	<div class="column between">
-		<h1 class="">{{ config('const.SITE_TITLE') }}</h1>
-		<p>{{ config('const.SITE_SUBTITLE') }}</p>
+		<a href="{{ asset('/') }}">
+			<h1 class="">{{ config('const.SITE_TITLE') }}</h1>
+			<p>{{ config('const.SITE_SUBTITLE') }}</p>
+		</a>
 	</div>
 </header>
