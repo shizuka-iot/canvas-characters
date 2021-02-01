cd8396bdb70f45e293a2ad383653df7eaff13861
shizuka-iot <tatakitubusu@gmail.com>
2021-02-01 12:34:35 +0900

    サーバーのルートを取り出してjsに渡すようにした

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -2,3 +2,6 @@
 <title>{{ config('const.SITE_TITLE') }}</title>
 <link rel="stylesheet" href="{{ asset('css/all.css') }}">
 <script src="{{ asset('js/app.js') }}"></script>
+<script>
+	const public_path = '{{ asset('/') }}';
+</script>
