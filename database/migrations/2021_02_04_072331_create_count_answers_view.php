b14e5cb6d353f83cc191c1417fa59f07184357ad
shizuka-iot <tatakitubusu@gmail.com>
2021-02-04 18:21:30 +0900

    質問に対する答えの数を求めるviewを作成するためのmigrationファイルを作成した

---
+++ database/migrations/2021_02_04_072331_create_count_answers_view.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountAnswersView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+			// answersテーブルにquesitonsテーブルをjoinし、
+			// questions.questionに対するanswersの数を求めるviewを作るSQL
+			$sql = 'create view count_answers_view as 
+				select question_id, questions.question, count(*) as answers 
+					from answers
+					join questions on question_id = questions.id 
+					group by question_id';
+			DB::statement($sql);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+			DB::statement('drop view count_answers_view');
+    }
+}
