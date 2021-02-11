03f7f488ab1a5ae6049fdfd42121f9af113bd98b
shizuka-iot <tatakitubusu@gmail.com>
2021-02-12 04:21:06 +0900

    利用規約ページを作成。中身はまだ記述していない

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	@component ('components.head')
+	@endcomponent
+</head>
+
+<body>
+	@component ('components.header')
+	@endcomponent
+
+	<main class="column center">
+	<!-- TODO: 利用規約 -->
+	</main>
+
+	@component ('components.footer')
+	@endcomponent
+</body>
+</html>
