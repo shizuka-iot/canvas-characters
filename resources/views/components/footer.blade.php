4d66d767e72c86eac8f0e9a2a5f569ee97404cc3
shizuka-iot <tatakitubusu@gmail.com>
2021-02-11 21:19:02 +0900

    フッターに利用規約へのリンクやコピーライトを配置

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,2 +1,10 @@
 
-<footer></footer>
+<footer class="column center">
+	<ul>
+		<li class="fs14"><a href="">利用規約</a></li>
+		<li class="fs14"><a href="">お問い合わせ</a></li>
+		<li class="fs14"><a href="">キャンバス・キャラクターズとは？</a></li>
+	</ul>
+
+	<p>©SHIZUKA-IOT</p>
+</footer>
