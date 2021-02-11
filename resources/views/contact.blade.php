a4c09c46b3b513467e58abb65a8495d3d09f5585
shizuka-iot <tatakitubusu@gmail.com>
2021-02-12 04:20:36 +0900

    お問い合わせページを作成。中身はまだ記述していない

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	@component ('components.head')
+	@endcomponent
+</head>
+
+<body>
+	@component ('components.header')
+	@endcomponent
+
+	<main class="column center">
+	<!-- TODO: お問い合わせ -->
+	</main>
+
+	@component ('components.footer')
+	@endcomponent
+</body>
+</html>
