40bfdeaa56194436261523149c59982104ab66e3
shizuka-iot <tatakitubusu@gmail.com>
2021-02-11 21:23:34 +0900

    いちいちcssをコンパイルするのが面倒なので一時的にstyles.cssを直接読み込むように変更

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>{{ config('const.SITE_TITLE') }}</title>
-<link rel="stylesheet" href="{{ asset('css/all.css') }}">
+<link rel="stylesheet" href="{{ asset('css/vender/styles.css') }}">
 <script src="{{ asset('js/app.js') }}"></script>
 <script>
 	const public_path = '{{ asset('/') }}';
