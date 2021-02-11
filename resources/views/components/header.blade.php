15d8d991efdc5e1bf75109ac2efcd26336692722
shizuka-iot <tatakitubusu@gmail.com>
2021-02-11 21:20:55 +0900

    フッターのデザインを変更。 タイトルの左右に画像を配置した

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,17 @@
 
 <header class="center column">
-	<div class="column between">
-		<a href="{{ asset('/') }}">
-			<h1 class="">{{ config('const.SITE_TITLE') }}</h1>
-			<p>{{ config('const.SITE_SUBTITLE') }}</p>
-		</a>
-	</div>
+		<div class="header_imgs row">
+			<img src="{{ asset('/imgs/fs01.png')}}" alt="">
+			<img src="{{ asset('/imgs/fs02.png')}}" alt="">
+			<img src="{{ asset('/imgs/fs03.png')}}" alt="">
+			<div class="column center">
+				<a href="{{ asset('/') }}">
+					<h1 class="">{{ config('const.SITE_TITLE') }}</h1>
+					<p>{{ config('const.SITE_SUBTITLE') }}</p>
+				</a>
+			</div>
+			<img src="{{ asset('/imgs/fs04.png')}}" alt="">
+			<img src="{{ asset('/imgs/fs05.png')}}" alt="">
+			<img src="{{ asset('/imgs/fs01.png')}}" alt="">
+		</div>
 </header>
