94688eb0172fc9ecc42a47eb5fa0d55d6a32502f
shizuka-iot <tatakitubusu@gmail.com>
2021-02-12 04:22:38 +0900

    利用規約・お問い合わせ・キャンバスキャラクターズについてのページリンクを作成. 。今まではリンク先を指定していなかった

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,9 +1,9 @@
 
 <footer class="column center">
 	<ul>
-		<li class="fs14"><a href="">利用規約</a></li>
-		<li class="fs14"><a href="">お問い合わせ</a></li>
-		<li class="fs14"><a href="">キャンバス・キャラクターズとは？</a></li>
+		<li class="fs14"><a href="{{ asset('/terms') }}">利用規約</a></li>
+		<li class="fs14"><a href="{{ asset('/contact') }}">お問い合わせ</a></li>
+		<li class="fs14"><a href="{{ asset('/about') }}">キャンバス・キャラクターズとは？</a></li>
 	</ul>
 
 	<p>©SHIZUKA-IOT</p>
