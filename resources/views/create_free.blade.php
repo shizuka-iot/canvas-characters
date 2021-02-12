252396953478b9928c3c61d4fef9972b2d0e7e83
shizuka-iot <tatakitubusu@gmail.com>
2021-02-13 07:49:17 +0900

    コンテンツが少ない時にフッターが下に固定されるようにした

--- resources/views/create_free.blade.php
+++ resources/views/create_free.blade.php
@@ -7,34 +7,37 @@
 </head>
 
 <body>
-	@component ('components.header')
-	@endcomponent
+	<div id="wrapper">
+		@component ('components.header')
+		@endcomponent
 
-	<main class="row">
-		<div class="">
-			<div class="row center" id="save_btn_wrapper">
-				<button id="save_chara_btn" type="button">保存する</button>
-			</div>
+		<main class="row">
+			<div class="">
+				<div class="sticky">
 
-			<div class="sticky">
+					<div id="canvas_wrapper" class="row center">
+						<canvas id="can" width="800" height="640">
+					</div>
 
-				<div id="canvas_wrapper" class="row center">
-					<canvas id="can" width="800" height="640">
+					<div class="row center" id="save_btn_wrapper">
+						<button id="save_chara_btn" type="button">保存する</button>
+					</div>
+
 				</div>
 
 			</div>
-		</div>
 
-		<aside>
-			@component ('components.select_parameters')
-			@endcomponent
-		</aside>
+			<aside>
+				@component ('components.select_parameters')
+				@endcomponent
+			</aside>
 
-	</main>
-	
-	@component ('components.footer')
-	@endcomponent
+		</main>
+		
+		@component ('components.footer')
+		@endcomponent
 
+	</div>
 	<script src="{{ asset('js/functions.js') }}"></script>
 	<script src="{{ asset('js/answers.js') }}"></script>
 	<script src="{{ asset('js/coordinates.js') }}"></script>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -8,28 +8,30 @@
 </head>
 
 <body>
-	@component ('components.header')
-	@endcomponent
+	<div id="wrapper">
+		@component ('components.header')
+		@endcomponent
 
-	<main class="column center">
-		<div class="select_mode row between">
-			<div class="left column center">
-				<img src="{{ asset('/imgs/fs_left.png')}}" alt="">
-				<a href="{{ asset('/question') }}" class="select_mode_btn">
-					<h2>診断する</h2>
-				</a>
+		<main class="column center">
+			<div class="select_mode row between">
+				<div class="left column center">
+					<img src="{{ asset('/imgs/fs_left.png')}}" alt="">
+					<a href="{{ asset('/question') }}" class="select_mode_btn">
+						<h2>診断する</h2>
+					</a>
+				</div>
+
+				<div class="right column center">
+					<img src="{{ asset('/imgs/fs_right.png')}}" alt="">
+					<a href="{{ asset('/create_free') }}" class="select_mode_btn">
+						<h2>自由に作る</h2>
+					</a>
+				</div>
 			</div>
+		</main>
 
-			<div class="right column center">
-				<img src="{{ asset('/imgs/fs_right.png')}}" alt="">
-				<a href="{{ asset('/create_free') }}" class="select_mode_btn">
-					<h2>自由に作る</h2>
-				</a>
-			</div>
-		</div>
-	</main>
-
-	@component ('components.footer')
-	@endcomponent
+		@component ('components.footer')
+		@endcomponent
+	</div>
 </body>
 </html>
