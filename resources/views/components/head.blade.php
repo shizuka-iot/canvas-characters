f815ef80f4931f34fb14b5c0d17ad4a482b6dafd
shizuka-iot <tatakitubusu@gmail.com>
2021-02-13 05:56:32 +0900

    googleのwebフォントを追加

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -4,6 +4,7 @@
 <link rel="stylesheet" href="{{ asset('css/vender/styles.css') }}">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200&display=swap" rel="stylesheet">
 <script src="{{ asset('js/app.js') }}"></script>
 <script>
 	const public_path = '{{ asset('/') }}';
