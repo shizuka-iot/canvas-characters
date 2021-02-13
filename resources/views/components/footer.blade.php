863fa47cb0035fd8f7e5dcd958592cc47a2eb8fd
shizuka-iot <tatakitubusu@gmail.com>
2021-02-13 15:57:30 +0900

    プライバシーポリシーページへのリンク作成

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,6 +3,7 @@
 	<div class="row between footer_contents">
 		<ul>
 			<li class="fs14"><a href="{{ asset('/terms') }}">利用規約</a></li>
+			<li class="fs14"><a href="{{ asset('/privacy') }}">プライバシーポリシー</a></li>
 			<li class="fs14"><a href="{{ asset('/contact') }}">お問い合わせ</a></li>
 			<li class="fs14"><a href="{{ asset('/about') }}">キャンバス・キャラクターズとは？</a></li>
 		</ul>
