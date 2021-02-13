767b021debef0a54da0e31b353279a43ccddfbbb
shizuka-iot <tatakitubusu@gmail.com>
2021-02-13 15:55:25 +0900

    プライバシーポリシーページを作成したのでルーティングを行った

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/create_free', 'CreateFreeController@index');
 Route::get('/about', function () {return view('about');});
 Route::get('/terms', function () {return view('terms');});
 Route::get('/contact', function () {return view('contact');});
+Route::get('/privacy', function () {return view('privacy');});
 Route::post('/_ajax', 'UpdateQuestionController@post');
