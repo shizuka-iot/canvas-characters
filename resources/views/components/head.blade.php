b30823f0c0802be2d62f87ff1c5d6c513b6b1142
shizuka-iot <tatakitubusu@gmail.com>
2021-02-16 06:20:24 +0900

    ビューポートの設定を記述

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,5 +1,6 @@
 <meta charset="UTF-8">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>{{ config('const.SITE_TITLE') }}</title>
 <link rel="stylesheet" href="{{ asset('css/vender/styles.css') }}">
 <link rel="preconnect" href="https://fonts.gstatic.com">
