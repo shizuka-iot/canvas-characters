24f9ecea84fb0269ab8199bcdef41d16eebc7271
shizuka-iot <tatakitubusu@gmail.com>
2021-02-15 20:17:53 +0900

    リンクの並び替えを変更

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,10 +2,10 @@
 <footer class="column center">
 	<div class="row between footer_contents">
 		<ul>
+			<li class="fs14"><a href="{{ asset('/about') }}">キャンバス・キャラクターズとは？</a></li>
 			<li class="fs14"><a href="{{ asset('/terms') }}">利用規約</a></li>
 			<li class="fs14"><a href="{{ asset('/privacy') }}">プライバシーポリシー</a></li>
 			<li class="fs14"><a href="{{ asset('/contact') }}">お問い合わせ</a></li>
-			<li class="fs14"><a href="{{ asset('/about') }}">キャンバス・キャラクターズとは？</a></li>
 		</ul>
 		<div class="twitter_icon_wrapper center">
 			<a href="https://twitter.com/tatakitsubusu">
