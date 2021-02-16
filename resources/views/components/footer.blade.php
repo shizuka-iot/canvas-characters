c3b5bdd2fb3f38a767c170589193bc39cd2ca482
shizuka-iot <tatakitubusu@gmail.com>
2021-02-17 00:36:02 +0900

    トップページへのリンクを追加

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,6 +6,7 @@
 			<li class="fs14"><a href="{{ asset('/terms') }}">利用規約</a></li>
 			<li class="fs14"><a href="{{ asset('/privacy') }}">プライバシーポリシー</a></li>
 			<li class="fs14"><a href="{{ asset('/contact') }}">お問い合わせ</a></li>
+			<li class="fs14"><a href="{{ asset('/') }}">トップページ</a></li>
 		</ul>
 		<div class="twitter_icon_wrapper center">
 			<a href="https://twitter.com/tatakitsubusu">
