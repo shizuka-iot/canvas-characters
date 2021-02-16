35a4877e52ac52e072d01484c55717db8404f7fd
shizuka-iot <tatakitubusu@gmail.com>
2021-02-16 23:14:31 +0900

    ヘッダーロゴの周囲の画像を囲むクラス名を修正

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,18 +2,24 @@
 <header class="center column">
 		<div class="header-line">
 		</div>
-		<div class="header_imgs row">
-			<img src="{{ asset('/imgs/fs01.png')}}" alt="">
-			<img src="{{ asset('/imgs/fs02.png')}}" alt="">
-			<img src="{{ asset('/imgs/fs03.png')}}" alt="">
+		<div class="header_imgs_wrapper column center">
+			<div class="left_header_imgs">
+				<img src="{{ asset('/imgs/fs01.png')}}" alt="">
+				<img src="{{ asset('/imgs/fs02.png')}}" alt="">
+				<img src="{{ asset('/imgs/fs03.png')}}" alt="">
+			</div>
+
 			<div class="column center">
 				<a href="{{ asset('/') }}">
 					<h1 class="">{{ config('const.SITE_TITLE') }}</h1>
 					<p>{{ config('const.SITE_SUBTITLE') }}</p>
 				</a>
 			</div>
-			<img src="{{ asset('/imgs/fs04.png')}}" alt="">
-			<img src="{{ asset('/imgs/fs05.png')}}" alt="">
-			<img src="{{ asset('/imgs/fs01.png')}}" alt="">
+
+			<div class="right_header_imgs">
+				<img src="{{ asset('/imgs/fs04.png')}}" alt="">
+				<img src="{{ asset('/imgs/fs05.png')}}" alt="">
+				<img src="{{ asset('/imgs/fs01.png')}}" alt="">
+			</div>
 		</div>
 </header>
