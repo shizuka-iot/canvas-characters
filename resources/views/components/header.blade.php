586dfebf025fc52518c1507e033510133352ba7f
shizuka-iot <tatakitubusu@gmail.com>
2021-02-19 06:21:34 +0900

    ヘッダーロゴをcssで操作するためにクラスをつけた

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -9,7 +9,7 @@
 				<img src="{{ asset('/imgs/logo_face/f03.png')}}" alt="">
 			</div>
 
-			<div class="column center">
+			<div class="header_logo_wrapper column center">
 				<a href="{{ asset('/') }}">
 					<h1 class="">{{ config('const.SITE_TITLE') }}</h1>
 					<p>{{ config('const.SITE_SUBTITLE') }}</p>
