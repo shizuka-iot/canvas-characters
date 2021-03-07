f92b714d84286806cc30730ad7551ee86c89f641
shizuka-iot <tatakitubusu@gmail.com>
2021-03-07 20:40:44 +0900

    お問い合わせ用コントローラー作成

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+	public function index()
+	{
+	}
+
+	public function confirm(Request $request)
+	{
+	}
+
+	public function send(Request $request)
+	{
+	}
+}
