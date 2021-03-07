0f96318da417386d2233f5822aa49b641d4d63f3
shizuka-iot <tatakitubusu@gmail.com>
2021-03-07 23:29:46 +0900

    お問い合わせフォームのコントローラーを記述。index()とconfirm()まで

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -8,10 +8,23 @@ class ContactController extends Controller
 {
 	public function index()
 	{
+		return view('contact.index');
 	}
 
 	public function confirm(Request $request)
 	{
+		$request->validate([
+			'email' => 'required|email',
+			'title' => 'required',
+			'body' => 'required',
+		]);
+
+		$inputs = $request->all();
+
+		return view('contact.confirm', [
+			'inputs'=>$inputs
+		]);
+
 	}
 
 	public function send(Request $request)
