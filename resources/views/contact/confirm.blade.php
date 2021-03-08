6594a4d18fadba8145db8a96250fbff9e5b8b9ef
shizuka-iot <tatakitubusu@gmail.com>
2021-03-08 16:09:29 +0900

    素の状態だったのでデザインを整えた。まだ完璧ではないので要改善

--- resources/views/contact/confirm.blade.php
+++ resources/views/contact/confirm.blade.php
@@ -15,34 +15,44 @@
 
 			<!-- お問い合わせ -->
 			<div class="container column center">
-				<h2>お問い合わせ</h2>
+				<h2>お問い合わせ内容確認画面</h2>
 
-				<form class="column" method="POST" action="{{ route('contact.send') }}">
+				<form id="contact_form" class="column" method="POST" action="{{ route('contact.send') }}">
 					@csrf
 
 
-					<label for="">メールアドレス</label>
-					{{ $inputs['email'] }}
-					<input type="hidden" name="email" value="{{ $inputs['email'] }}">
+					<div class="confirm_input_items_wrapper">
+						<label for="">メールアドレス</label>
+						<span class="confirm_input_item">{{ $inputs['email'] }}</span>
+						<input type="hidden" name="email" value="{{ $inputs['email'] }}">
+					</div>
 
 
-					<label for="">タイトル</label>
-					{{ $inputs['title'] }}
-					<input type="hidden" name="title" value="{{ $inputs['title'] }}">
+					<div class="confirm_input_items_wrapper">
+						<label for="">タイトル</label>
+						<span class="confirm_input_item">{{ $inputs['title'] }}</span>
+						<input type="hidden" name="title" value="{{ $inputs['title'] }}">
+					</div>
 
 
-					<label for="">お問い合わせ内容</label>
-					{!! nl2br(e($inputs['body'])) !!}
-					<input type="hidden" name="body" value="{{ $inputs['body'] }}">
+					<div class="confirm_input_items_wrapper">
+						<label for="">お問い合わせ内容</label>
+						<span class="confirm_input_item">{!! nl2br(e($inputs['body'])) !!}</span>
+						<input type="hidden" name="body" value="{{ $inputs['body'] }}">
+					</div>
 
 
-					<button type="submit" name="action" value="back">
-						入力内容修正
-					</button>
+					<div class="contact_fix_button center">
+						<button type="submit" name="action" value="back">
+							入力内容修正
+						</button>
+					</div>
 
-					<button type="submit" name="action" value="submit">
-						送信する
-					</button>
+					<div class="confirm_button center">
+						<button type="submit" name="action" value="submit">
+							送信する
+						</button>
+					</div>
 
 				</form>
 			</div>
